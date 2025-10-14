61d059559ece8096caa49717747b25156cbcea0f
UDAYNMURTHY13 <135510278+UDAYNMURTHY13@users.noreply.github.com>
2025-10-14 16:12:30 +0530

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 @st.cache_resource
 def load_model():
     with open('models/best_attrition_model.pkl', 'rb') as f:
-    model = pickle.load(f)
+        model = pickle.load(f)
 
     with open('models/feature_scaler.pkl', 'rb') as f:
         scaler = pickle.load(f)
