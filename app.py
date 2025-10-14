12a77043965cde4580cc8d06367816b1a00bf948
UDAYNMURTHY13 <135510278+UDAYNMURTHY13@users.noreply.github.com>
2025-10-14 16:11:51 +0530

    app.py

--- app.py
+++ app.py
@@ -7,15 +7,14 @@ import pandas as pd
 # Load model, scaler, and feature names
 @st.cache_resource
 def load_model():
-    with open('models\best_attrition_model.pkl', 'rb') as f:
-        model = pickle.load(f)
-    
+    with open('models/best_attrition_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
     with open('models/feature_scaler.pkl', 'rb') as f:
         scaler = pickle.load(f)
 
     with open('models/feature_names.pkl', 'rb') as f:
         feature_names = pickle.load(f)
-
     return model, scaler, feature_names
 
 model, scaler, feature_names = load_model()
